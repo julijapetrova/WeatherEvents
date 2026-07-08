3a1d7e80c3dcdb04431cb7aa8fa38b89ab16481c
J P <jule.p3t@gmail.com>
2026-07-08 20:28:15 +0200

    Setup logging

--- WeatherEvents/Program.cs
+++ WeatherEvents/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -10,6 +14,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation(
+    "WeatherEvents API started successfully at {Time}",
+    DateTime.UtcNow);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -27,3 +37,26 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+app.UseExceptionHandler(exceptionApp =>
+{
+    exceptionApp.Run(async context =>
+    {
+        var logger = context.RequestServices
+            .GetRequiredService<ILogger<Program>>();
+
+        var exceptionHandler =
+            context.Features.Get<IExceptionHandlerFeature>();
+
+        if (exceptionHandler?.Error != null)
+        {
+            logger.LogError(
+                exceptionHandler.Error,
+                "Unhandled exception occurred while processing request");
+        }
+
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync(
+            "An unexpected error occurred.");
+    });
+});
